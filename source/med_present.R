e43a8f0c509b7575e1cf4251c65e3102cc9dbad4
vishal2297thakur <vishu2297thakur@gmail.com>
2023-03-22 13:13:10 +0000

    testing

--- source/med_present.R
+++ source/med_present.R
@@ -2,3 +2,5 @@ OUTFILES_DIR <- "../shared/data_projects/med_datasets/2000_2019_data" #rename to
 STUDY_PERIOD_START <- as.Date("2000-01-01")
 STUDY_PERIOD_END <- as.Date("2019-12-31")
 STUDY_AREA <- extent(-10.25, 40.25, 29.75, 45.25) 
+
+#Testing the example of medi.
