722c2e4d81d6138a5e8806eafc2250b4c73ce8d4
imarkonis <imarkonis@gmail.com>
2023-03-22 14:55:27 +0200

    testing

--- source/med_present.R
+++ source/med_present.R
@@ -1,4 +1,4 @@
-OUTFILES_DIR <- "../shared/data_projects/med_datasets/2000_2019_data"
+OUTFILES_DIR <- "../shared/data_projects/med_datasets/2000_2019_data" #rename to PATH_ convention
 STUDY_PERIOD_START <- as.Date("2000-01-01")
 STUDY_PERIOD_END <- as.Date("2019-12-31")
-STUDY_AREA <- extent(-10.25, 40.25, 29.75, 45.25)
+STUDY_AREA <- extent(-10.25, 40.25, 29.75, 45.25) 
