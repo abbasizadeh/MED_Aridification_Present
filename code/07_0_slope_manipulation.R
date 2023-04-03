c404173c060085cbe52b0ca1b8c13b14bf679be4
abbasizadeh <hossein.abbasizadeh@gmail.com>
2023-04-03 14:55:16 +0000

    modify slope scripts

--- code/07_0_slope_manipulation.R
+++ code/07_0_slope_manipulation.R
@@ -4,10 +4,11 @@ source("./code/source/global_variables.R")
 # load functions
 source('./code/source/functions.R')
 
-path_slope_files <- ("~/shared/data_projects/med_datasets/2000_2019_data/slopes_p_minus_e/")
-budyko_data <- readRDS( "~/shared/data_projects/med_datasets/2000_2019_data/sim/budyko/evaporative_aridity_indices/budyko_data_04_1.rds")
-save_dir <-  "~/shared/data_projects/med_datasets/2000_2019_data/sim/budyko/evaporative_aridity_indices/"
 
+path_slope_files <- "~/shared/data_projects/med_datasets/2000_2019_data/slopes_p_minus_e/"
+path_save <-  "~/shared/data_projects/med_datasets/2000_2019_data/budyko/"
+
+budyko_data <- readRDS( "~/shared/data_projects/med_datasets/2000_2019_data/budyko/04_1_budyko_data_joint_entropy.rds")
 
 
 unique(budyko_data$combination)
@@ -29,7 +30,7 @@ for (i in 1:length(slope_files)) {
   name_dummie <- read.table(text = slope_files[i], sep = "_", as.is = TRUE)
   
   # create combination name using the name of each file
-  combination_dummie <- paste0("pet", "_", name_dummie$V5, "_", "p", "_", name_dummie$V2)
+  combination_dummie <- paste0("e_pet_", name_dummie$V5, "_p_", name_dummie$V2)
   
   # add the combination name to the data frame
   raster_data_frame_dummie$combination <- rep(combination_dummie, length(raster_data_frame_dummie$x))
@@ -56,14 +57,14 @@ names(slope_data) <- c('x', 'y', 'slope', 'combination')
 
 slope_budyko_data <- merge(budyko_data, slope_data, by = c('x', 'y', 'combination'))
 
-slope_budyko_data[combination == "pet_terraclimate_p_persiann"]
-slope_data[combination == "pet_terraclimate_p_persiann"]
-budyko_data[combination == "pet_terraclimate_p_persiann"]
+slope_budyko_data[combination == "e_pet_terraclimate_p_persiann"]
+slope_data[combination == "e_pet_terraclimate_p_persiann"]
+budyko_data[combination == "e_pet_terraclimate_p_persiann"]
 
 unique(slope_budyko_data$combination)
 unique(slope_data$combination)
 unique(budyko_data$combination)
 
 
-saveRDS(slope_budyko_data, paste0(save_dir, "slope_budyko_data_07.rds"))
+saveRDS(slope_budyko_data, paste0(path_save, "07_slope_entropy_budyko_data.rds"))
 
